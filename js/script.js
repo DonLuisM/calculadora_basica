9be84740702e42864d1f4e1887708cd010a48f60
DonLuisM <lfmb03@outlook.com>
2025-01-09 09:03:20 -0500

    updates_css_js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,16 @@ const display = document.getElementById("display")
 buttons.forEach(
     button => button.addEventListener("click", () => {
         const value = button.textContent
-        document.getElementById("display").textContent += value
+
+        if (button.id === "C"){
+            display.textContent = "0";
+            return;
+        }
+
+        if (display.textContent === "0"){
+            display.textContent = value
+        } else {
+            display.textContent += value;
+        }
     })
 )
