ebd14d57fb58f1c36d1895cf7417589858a0a45e
DonLuisM <lfmb03@outlook.com>
2025-01-10 19:57:11 -0500

    calculadora_updates

--- js/script.js
+++ js/script.js
@@ -1,19 +1,38 @@
-const buttons = document.querySelectorAll(".boton")
-const display = document.getElementById("display")
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll(".boton");
 
-buttons.forEach(
-    button => button.addEventListener("click", () => {
-        const value = button.textContent
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const botonApretado = button.textContent.trim();
 
-        if (button.id === "C"){
+        if (button.id === "C") {
             display.textContent = "0";
             return;
         }
 
-        if (display.textContent === "0"){
-            display.textContent = value
+        if (button.id === "=") {
+            display.textContent = eval(display.textContent);
+            return;
+        }
+
+        if (display.textContent === "0") {
+            display.textContent = botonApretado;
         } else {
-            display.textContent += value;
+            display.textContent += botonApretado;
         }
     })
-)
+})
+
+function cambiarColor(modo) {
+    const body = document.querySelector('body');
+
+    // Verificar si el fondo es oscuro o claro
+    if (modo === 'light_mode') {
+        body.classList.remove('dark_mode');
+        body.classList.add('light_mode');
+    } else {
+        body.classList.remove('light_mode');
+        body.classList.add('dark_mode');
+    }
+}
+
